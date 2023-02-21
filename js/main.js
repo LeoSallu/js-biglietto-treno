4c4e292b8ff8ba21fdf4a46706da782e45f2ff3a
LeoSallu <L.sallustio99@gmail.com>
2023-02-21 14:05:23 +0100

    calcolo prezzo per km

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@
 
 // CHIEDERE KM DA PERCORRERE
 
-const kmTot = Number(prompt("Quanti km vuoi percorrere ?"));
-const kmPercorsi=(kmTot+' Km')
-console.log(kmPercorsi);
+const kmPercorsi = Number(prompt("Quanti km vuoi percorrere ?"));
+console.log(kmPercorsi + ' km');
 
 //CHIEDERE ETÁ
 
 const età = Number(prompt("Quanti anni hai ?"));
-const etàTot = (età+' anni')
-console.log(etàTot)
+console.log(età + ' anni');
+
+//CALCOLO DEL PREZZO
+
+const prezzoPerKm = (kmPercorsi * 0.21);
+console.log(prezzoPerKm + ' €');
