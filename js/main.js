da24199f0113814b58b1b4459a5328f66bfb4f48
LeoSallu <L.sallustio99@gmail.com>
2023-02-21 14:31:19 +0100

    impostazione sconti ma problema formula

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // CHIEDERE KM DA PERCORRERE
 
 const kmPercorsi = Number(prompt("Quanti km vuoi percorrere ?"));
@@ -11,6 +10,20 @@ const età = Number(prompt("Quanti anni hai ?"));
 console.log(età + ' anni');
 
 //CALCOLO DEL PREZZO
+    //-Prezzo per Km
 
 const prezzoPerKm = (kmPercorsi * 0.21);
 console.log(prezzoPerKm + ' €');
+
+    //-Sconto per età
+if(età<18){
+    const prezzoPerKmScontato = (((prezzoPerKm / 100) - 20).toFixed(2));
+    console.log(prezzoPerKmScontato + ' €');
+} else if(età>=65){
+    const prezzoPerKmScontato = (((prezzoPerKm / 100) - 40).toFixed(2));
+    console.log(prezzoPerKmScontato + ' €');
+}
+else{
+    const prezzoPerKm = (kmPercorsi * 0.21);
+    console.log(prezzoPerKm + ' €');
+}
