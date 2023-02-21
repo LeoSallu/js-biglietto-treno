171298c9e694fc1482d8c82744f69f2162e74c04
LeoSallu <L.sallustio99@gmail.com>
2023-02-21 13:54:35 +0100

    Srutturati primi due prompt

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// CHIEDERE KM DA PERCORRERE
+
+const kmTot = Number(prompt("Quanti km vuoi percorrere ?"));
+const kmPercorsi=(kmTot+' Km')
+console.log(kmPercorsi);
+
+//CHIEDERE ETÁ
+
+const età = Number(prompt("Quanti anni hai ?"));
+const etàTot = (età+' anni')
+console.log(etàTot)
